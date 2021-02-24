d14c87d2d90514ee6c2fa6c409480360e0ff7f57
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-02-24 20:49:03 +0530

    Add files via upload

---
+++ 2.Insertion of Node and display.cpp
@@ -0,0 +1,82 @@
+// 2. Insertion of Node and display list
+// Author: Akshay A Baiju
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+typedef struct node		//structure of node
+{
+	int data;
+	struct node *next;
+}node;
+
+node *createNode()		//dynamic allocation of memory for a new node created
+{
+	node *temp;
+	temp=(node*)malloc(sizeof(node));
+	return temp;
+}
+
+node *head=NULL;	//global pointer variable to store address of first node(initially NULL)
+
+void insert()
+{
+	system("cls");
+	node *temp=createNode();
+	printf("Enter data into node : ");
+	scanf("%d",&temp->data);
+	temp->next=NULL;
+	if (head==NULL)		//to store the address of first node into head pointer
+	head=temp;
+	else
+	{
+		node *ref=head;		//reference pointer is used to traverse the linked list, initially pointing first node
+		while (ref->next!=NULL)		//to move reference to point last node
+		ref=ref->next;
+		ref->next=temp;		//to store address of newly created node at the next address of last node of Linked List, this automatically connects the last node to the newly created node
+	}
+}
+
+void display()
+{
+	system("cls");
+	if (head==NULL)
+	printf("List is currently empty \n");
+	else
+	{
+		node *ref=head;
+		printf("%d->",ref->data);
+		while(ref->next!=NULL)	
+		{
+			ref=ref->next;
+			printf("%d->",ref->data);
+		}	
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int choice,p=0;
+	char ch;
+	do
+	{
+		system("cls");
+		printf("1. to insert a node to List \n");
+		printf("2. to display List \n");
+		printf("3. to exit \n");
+		printf("Enter choice <1/2/3> : ");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:insert();break;
+			case 2:display();break;
+			case 3:p=1;break;
+			default:printf("Enter a valid choice !!!");
+		}
+		printf("\nPress enter to continue...");
+		getch();
+	}
+	while (p==0);
+	return 0;
+}
